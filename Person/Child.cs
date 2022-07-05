5f6c29d8ca5def4111560619a482c3c6043e3287
Nikolay Penkov <nikolay7penkov@gmail.com>
2022-07-05 22:11:57 +0300

    Add project files.

---
+++ Person/Child.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+        public override string Name 
+        {
+            get
+            {
+                return base.Name;
+            } 
+            set
+            {
+                base.Name = value;
+            }
+        }
+        public override int Age { get; set; }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
---
+++ Person/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public Person(string name, int age)
+        {
+            this.Name= name;
+            this.Age = age;
+        }
+        public virtual string Name { get; set; }
+        public virtual int Age { get; set; }
+    }
+}

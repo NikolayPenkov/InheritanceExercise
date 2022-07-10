fbe71aeb85a557a18125d7a87b35b13155bcf889
Nikolay Penkov <97218864+NikolayPenkov@users.noreply.github.com>
2022-07-10 16:12:26 +0300

    Add files via upload

---
+++ NeedForSpeed/Cake.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        const double CakeGrams = 250;
+        const double CakeCalories = 1000;
+        const decimal CakePrice = 5m;
+        public Cake(string name) : base(name, CakePrice, CakeGrams, CakeCalories)
+        {
+        }
+    }
+}
